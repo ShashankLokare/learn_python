91333206756f195db67538ad419c18c30f70f297
Shashank Lokare <lokaress@gmail.com>
2024-05-24 15:53:25 +0200

    mod1

--- python_data_structures.py
+++ python_data_structures.py
@@ -1,9 +1,13 @@
 
-
-List1 = ['Geek','For','Geek']
+#List in python
+List1 = ['Geek','For','Geek',1 ,2 ,3 ]
 
 List2 = [['Geek','For'],['Geek']]
 
 print(List1[2])
 
 print(List2[1][0])
+List3 = [List1[3], "World", List2[1]]
+
+print("\nList dynamic\n")
+print(List3)
